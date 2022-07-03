ef8b6796aaf9ef95098fd92d12d1c9e67c6e01aa
Jayda <Jayda@LAPTOP-VCKUF425>
2022-07-03 21:48:51 +0800

    these are Calculators and CalculatorTest!!

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -10,6 +10,10 @@ public class Calculator {
 		return a * b;
 	}
 	public int divide (int a, int b) {
+		if (b == 0 ) {
+			throw new IllegalArgumentException("Division by zero is not supported");
+		}
+		
 		return a / b;
 	}
 
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -74,4 +74,20 @@ public void testDivide() {
 	assertEquals (expected, actual);
 
 }
+@Test
+public void testDivideWith0Denominator () {
+	
+	int a = 20;
+	int b = 0;
+	int c = 4;
+	try {
+		Calculator cal = new Calculator();
+		cal.divide(a, c);
+		fail("Expected an IllegalArgumentException to be thrown");
+	}catch(IllegalArgumentException e) {
+		assertEquals("Division by zero is not supported ", e.getMessage() );
+	}catch(Throwable t) {
+		assertEquals( "Expected an IllegalArgumentException to be thrown", t.getMessage());
+	}
+}
 }
